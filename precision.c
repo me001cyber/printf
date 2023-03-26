880d85385d5d71a4c983dfb9ce15e6c7e4e15ddc
me001cyber <76008783+me001cyber@users.noreply.github.com>
2023-03-26 14:11:46 +0000

    Done

--- precision.c
+++ precision.c
@@ -2,7 +2,7 @@
 
 /**
  * get_precision - Calculates the precision for printing "Done"
- * by Joseph and Ciny
+ * by me001cyber & Prometheus1994
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
