3f341050794acfeb12f70eba3709d5ba048c22d6
Prometheus1994 <48414632+Prometheus1994@users.noreply.github.com>
2023-03-26 13:46:06 +0000

    Done

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - Printf function "Done"
  * @format: format.
  * by Joseph and Ciny
  * Return: Printed chars.
@@ -54,7 +54,7 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exist "Done"
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
