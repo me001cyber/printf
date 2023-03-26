01087af0da3ce8b7d7e3cf745a1eabe264f325ad
me001cyber <76008783+me001cyber@users.noreply.github.com>
2023-03-26 14:13:48 +0000

    Done

--- size.c
+++ size.c
@@ -2,7 +2,7 @@
 
 /**
  * get_size - Calculates the size to cast the argument "Done"
- * by Joseph and Ciny
+ * by me001cyber & Prometheus1994
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * Return: Precision.
